40a0bafe129384ed30db8fbf9115543d1f1deeb1
Paul Fannon <lurchworld@gmail.com>
2018-05-21 15:15:58 -0500

    Search controller returns all jobs with a corresponding view.

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -4,6 +4,7 @@ import org.launchcode.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
@@ -23,5 +24,24 @@ public class SearchController {
     }
 
     // TODO #1 - Create handler to process search request and display results
-
+    @RequestMapping(value = "results")
+    public String searchResults(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
+        model.addAttribute("columns", ListController.columnChoices);
+        // HashMap<String, String> jobsList = new HashMap<>();
+        ArrayList<HashMap<String, String>> jobs = new ArrayList<>();
+        if (searchType.equals("all")) {
+            jobs = JobData.findAll();
+            //for (int i = 0; i < jobs.size(); i++) {
+            //    HashMap<String, String> hashJob = new HashMap<>();
+             //   hashJob = jobs.get(i);
+             //   for (Map.Entry<String, String> data : hashJob.entrySet()) {
+             //       jobsList.put(data.getKey(), data.getValue());
+             //   }
+            //}
+        }
+        model.addAttribute("columns", ListController.columnChoices);
+        model.addAttribute("title", "Search Results");
+        model.addAttribute("jobs", jobs);
+        return "search";
+    }
 }
